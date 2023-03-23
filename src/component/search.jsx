e8693894e5a88749c4bc02d6a80390629969b9c2
Sandeep Kumar M <sdagangstar11@gmail.com>
2023-03-23 16:39:52 +0530

    in note area i add hover style using 'scale' css property

--- src/component/search.jsx
+++ src/component/search.jsx
@@ -3,7 +3,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import { UserContext } from "./simplyfy";
 
 
-function Search({handleSearch,searchNotes}){
+function Search(){
 
     const {searchnotes,setsearchnotes} = useContext(UserContext);
 
@@ -20,8 +20,17 @@ function Search({handleSearch,searchNotes}){
         }
     }
 
+    function handleMouseOut(event){
+        const {value} = event.target;
+        if(value === ""){
+            setSearchBar(false);
+        } else {
+            setSearchBar(true);
+        }
+    }
+
     return(
-        <div onMouseOver={() => setSearchBar(true)} onMouseOut={() => setSearchBar(false)} className={`${searchBar ? "search-e" : "search"}`}>
+        <div onMouseOver={() => setSearchBar(true)} onMouseOut={handleMouseOut} className={`${searchBar ? "search-e" : "search"}`}>
             <div className="search-icon"><SearchIcon /></div>
             <input className="search-input" type="text" onChange={handleSearchChange} value={searchnotes} placeholder="Search"></input>
         </div>
